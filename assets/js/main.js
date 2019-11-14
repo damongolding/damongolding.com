482a38eec4c724205b4451cb7b1f240e44b17ce4
Damon <damon@damongolding.com>
2019-11-14 14:27:51 +0000

    animation changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   
   var tl = gsap.timeline({defaults:{ease: "power3.out"}});
 
- 
     tl.to("#introduction-animation-bg", {duration:0.7, width:'0%', delay:1, onComplete:() => {document.getElementById('introduction-animation').remove();}});
     tl.to("#introduction-animation-title", {duration:1, x:-100, opacity:0 }, "-=0.8");
     tl.from(["#introduction h1", "#introduction p"], {duration:0.5, x:"-100", opacity:0, stagger:0.2}, "-=0.5");
@@ -19,15 +18,17 @@ var pjax = new Pjax({
 
 
 document.addEventListener('pjax:complete', ()=>{
-  gsap.to('#pageloader', {duration:0.5, y:"100%", onComplete:()=>{
-    gsap.set('#pageloader', {y:"-100%"});
+
+  gsap.to('#pageloader', {duration:0.5, x:"-100%", onComplete:()=>{
+    gsap.set('#pageloader', {x:"100%"});
   }});
+  gsap.from('#content',{duration:0.5, x:"-100"});
   
 });
 
 
 function loadpage(e){
-  gsap.to('#pageloader', {duration:0.5, y:"0%", onComplete:()=>{
+  gsap.to('#pageloader', {duration:0.5, x:"0%", onComplete:()=>{
     pjax.loadUrl(e.target.href);
   }});
 }
